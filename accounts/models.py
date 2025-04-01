f7648ed9f8d3e20fcf3a34ef9ccb018821ae1c6f
seoyoung <1203sylim@gmail.com>
2025-04-01 14:08:26 +0900

    follow 기능을 위한 모델링

--- accounts/models.py
+++ accounts/models.py
@@ -10,3 +10,7 @@ class User(AbstractUser):
         crop=['middle', 'center'],
         upload_to='profile',
     )
+# 내가 팔로우 하는 사람
+    followings = models.ManyToManyField('self', related_name='followers', symmetrical=False)# 유저와 유저를 n:n으로 연결(스스로를) 팔로우 버튼 누른 사람 저장 버튼
+
+# 나를 팔로우 하는 사람
